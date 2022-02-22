e287c3329d5a3b76d45294235676079aa5fc1297
Dimitri <dimitri.tarn@gmail.com>
2022-02-22 12:12:30 +0100

    Eslint +Prettier

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from "@vitejs/plugin-legacy";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), legacy()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react"],
+          "react-dom": ["react-dom"],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
