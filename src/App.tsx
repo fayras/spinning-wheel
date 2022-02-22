8a849f4edee3497d154b8a10e21ee4cbd16a0a7e
Dimitri <dimitri.tarn@gmail.com>
2022-02-22 17:20:26 +0100

    Man kann nun den Store direkt über die Actions "spin" und "addItem" steuern

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import { Button } from "@chakra-ui/react";
-import { useState } from "react";
 import { Canvas } from "./components/Canvas";
-import { state } from "./state/wheel";
+import { spin, addItem } from "./state/wheel";
 
 export const App = () => {
-  const [count] = useState(0);
-
   return (
     <div>
-      <div>test: {count}</div>
-      <Button
-        onClick={() => {
-          state.rotationSpeed = 2;
-        }}
-      >
-        spin to win
-      </Button>
-      <Canvas height={100} width={100} />
+      <Button onClick={() => spin()}>spin to win</Button>
+      <Button onClick={() => addItem()}>add</Button>
+      <Canvas height={300} width={300} />
     </div>
   );
 };
--- src/state/wheel.ts
+++ src/state/wheel.ts
@@ -1,3 +1,31 @@
 import { proxy } from "valtio";
 
-export const state = proxy({ rotationSpeed: 2 });
+type WheelItem = {
+  id: number;
+  label: string;
+  color: number;
+};
+
+type State = {
+  rotationSpeed: number;
+  items: WheelItem[];
+};
+
+function createItem(label: string, color?: number): WheelItem {
+  const id = new Date().valueOf() + Math.random();
+
+  return { id, label, color: color || Math.random() * 0xffffff };
+}
+
+export const state = proxy<State>({
+  rotationSpeed: 0,
+  items: ["a", "b", "c"].map((i) => createItem(i)),
+});
+
+export const spin = () => {
+  state.rotationSpeed = 3;
+};
+
+export const addItem = () => {
+  state.items.push(createItem("a"));
+};
