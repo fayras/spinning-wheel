d55767ce8ec30a0f70d3969630eb3923db5f2d12
Dimitri <dimitri.tarn@gmail.com>
2022-02-23 16:13:51 +0100

    Indikator oben in der Mitte hinzugefügt

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,6 @@
 import { IApplicationOptions } from "pixi.js";
 import { Stage } from "react-pixi-fiber/index";
+import { Arc } from "./Arc";
 import { Wheel } from "./Wheel";
 
 type Props = {
@@ -18,6 +19,14 @@ export const Canvas = ({ width, height }: Props) => {
   return (
     <Stage options={OPTIONS}>
       <Wheel x={width / 2} y={height / 2} radius={width / 2 - width * 0.1} />
+      <Arc
+        x={width / 2}
+        y={width * 0.1 + 15}
+        radius={30}
+        startAngle={-0.4}
+        endAngle={0.4}
+        color={0xff0000}
+      />
     </Stage>
   );
 };
