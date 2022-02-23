720bc183717e16f7dc964904da9b43a03ace8d20
Dimitri <dimitri.tarn@gmail.com>
2022-02-23 18:46:21 +0100

    Vite Config aktualisiert:
    Base Pfad gesetzt, da dieser für GH Pages benötigt wird

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/spinning-wheel/",
   plugins: [react(), tsconfigPaths(), legacy()],
   build: {
     rollupOptions: {
