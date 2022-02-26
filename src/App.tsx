c8de174f39d35ff39d5b555fff8583aa26e6833e
Dimitri <dimitri.tarn@gmail.com>
2022-02-26 14:10:08 +0100

    TopBar hinzugefügt

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import {
-  Button,
-  Flex,
-  Box,
-  Center,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Flex, Box, Center, useColorModeValue } from "@chakra-ui/react";
 import { AddItem } from "./components/AddItem";
 import { Canvas } from "./components/Canvas";
 import { ItemsList } from "./components/ItemsList";
 import { ListSelect } from "./components/ListSelect";
 import { SpinButton } from "./components/SpinButton";
+import { TopBar } from "./components/TopBar";
 
 export const App = () => {
-  const { toggleColorMode } = useColorMode();
   const bg = useColorModeValue("gray.50", "gray.800");
 
   return (
-    <Flex backgroundColor={bg} minHeight="100vh">
-      <Center flexDirection="column">
-        <SpinButton />
-        <Canvas height={600} width={600} />
-      </Center>
-      <Box p="10" flex="1" maxW={500}>
-        <Box mb="10">
-          <Button onClick={() => toggleColorMode()}>toggle dark mode</Button>
+    <Flex backgroundColor={bg} minHeight="100vh" flexDirection="column">
+      <TopBar />
+      <Flex flexDirection="row">
+        <Center flexDirection="column">
+          <SpinButton />
+          <Canvas height={600} width={600} />
+        </Center>
+        <Box p="10" flex="1" maxW={500}>
+          <ListSelect />
+          <AddItem />
+          <ItemsList />
         </Box>
-        <ListSelect />
-        <AddItem />
-        <ItemsList />
-      </Box>
+      </Flex>
     </Flex>
   );
 };
