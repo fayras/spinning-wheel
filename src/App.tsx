1c5498166978c8238aa17b482e13f4db2ba49297
Dimitri <dimitri.tarn@gmail.com>
2022-03-04 21:05:43 +0100

    WIP: Gibt es noch keine Liste, wird ein Platzhalter und ein Button zum Anlegen von Listen angezeigt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import { Flex, Box, Center, useColorModeValue } from "@chakra-ui/react";
+import { useSnapshot } from "valtio";
+import { state } from "@/state/lists";
 import { AddItem } from "./components/AddItem";
 import { Canvas } from "./components/Canvas";
 import { ItemsList } from "./components/ItemsList";
 import { ListSelect } from "./components/ListSelect";
 import { SpinButton } from "./components/SpinButton";
 import { TopBar } from "./components/TopBar";
+import { CreateInitialList } from "./components/CreateInitialList";
+import { Waiting } from "./icons/Wating";
 
 export const App = () => {
   const bg = useColorModeValue("gray.50", "gray.800");
+  const lists = useSnapshot(state);
 
   return (
     <Flex backgroundColor={bg} minHeight="100vh" flexDirection="column">
       <TopBar />
       <Flex flexDirection="row">
         <Center flexDirection="column">
-          <SpinButton />
-          <Canvas height={600} width={600} />
+          {lists.hasLists ? (
+            <>
+              <SpinButton />
+              <Canvas height={600} width={600} />
+            </>
+          ) : (
+            <Box
+              height={600}
+              width={600}
+              p="20"
+              justifyContent="center"
+              textAlign="center"
+            >
+              <Waiting height="100%" width="100%" opacity={0.6} />
+              <Box fontSize="2xl">Add some items to your list!</Box>
+            </Box>
+          )}
         </Center>
         <Box p="10" flex="1" maxW={500}>
-          <ListSelect />
-          <AddItem />
-          <ItemsList />
+          {lists.hasLists ? (
+            <>
+              <ListSelect />
+              <AddItem />
+              <ItemsList />
+            </>
+          ) : (
+            <CreateInitialList />
+          )}
         </Box>
       </Flex>
     </Flex>
