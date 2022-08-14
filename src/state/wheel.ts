09b8d39e8b96864393651920c8de48f5aa4a6f49
Dimitri <dimitri.tarn@gmail.com>
2022-08-14 16:02:04 +0200

    Rotationsverhalten angepasst

--- src/state/wheel.ts
+++ src/state/wheel.ts
@@ -11,17 +11,38 @@ export const state = proxy<State>({
   activeItem: null,
 });
 
-let rotationSpeed = 0;
+let spinDuration = 0;
+let spinTime = 0;
+let spinBackMagnitude = 0;
 
-export const spin = (speed = 3) => {
-  rotationSpeed = speed + Math.random() * speed;
+export const spin = () => {
+  spinDuration = 2500 + Math.random() * 500;
+  spinBackMagnitude = Math.random() * 0.4;
+  spinTime = 0;
 };
 
-Ticker.shared.add((dt) => {
-  rotationSpeed *= 0.98;
-  if (rotationSpeed < 0.001) {
-    rotationSpeed = 0;
-  }
+function curve(
+  time: number,
+  beginningValue: number,
+  finalValue: number,
+  duration: number,
+  magnitude = 1.70158
+): number {
+  const t = time / duration - 1;
+  const c = finalValue - beginningValue;
+  const steepness = 0.1;
 
-  state.rotation += rotationSpeed * dt;
+  return (
+    (c * (t * t * ((magnitude + 1) * t + magnitude) + 1) + beginningValue) **
+    steepness
+  );
+}
+
+Ticker.shared.add((dt) => {
+  if (spinTime < spinDuration) {
+    const speed = 1 - curve(spinTime, 0, 1, spinDuration, spinBackMagnitude);
+
+    spinTime += dt;
+    state.rotation += speed * dt;
+  }
 });
